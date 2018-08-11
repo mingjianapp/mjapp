6bd7bab48bfa58ec28acc0ba64458cbd8d4704e8
mingjianapp <42273131+mingjianapp@users.noreply.github.com>
2018-08-11 13:58:13 +0000

    Update data.js

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ var feed_data = {
                },
                
    "shorturl": { "name": "短网址", 
-                 "value": [ "ZUM1amJ5OHlNamc0", "ZUM1amJ5OHlNamc0", "ZUM1amJ5OHlNalky"]
+                 "value": [ "ZUM1amJ5OHlNamc0", "ZUM1amJ5OHlNalky", "ZUM1amJ5OHlNalky"]
                }, 
    
   "ogateurl": { "name": "ogate",
@@ -18,7 +18,7 @@ var feed_data = {
                  "value": [ "TVRFMExqSTBMamd5TGpFek5BPT0=", "TVRFMExqSTBMamd5TGpFek5BPT0=", "WkRFMmNHaHFjV2wyZFhWbmRtUXVZMnh2ZFdSbWNtOXVkQzV1WlhRPQ=="]
                },
   "ogColl": { "name": "网门",
-                 "value": [ "ZUM1amJ5OHlNalky", "ZUM1amJ5OHlNalky", "YUd0a1ptbGphQzU0TG1Odkx6SXlOalk9"]
+                 "value": [ "ZUM1amJ5OHlNalky", "ZUM1amJ5OHlNalky", "YVdaNGRIQmhjeTU0TG1Odkx6SXlOalk9"]
                }, 
 
     "article": { "name": "最新文章", 
